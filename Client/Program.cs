4f821a66f45426f535d6c5ef3d4ea79c0438c53e
soloV <liujiawei_it@163.com>
2018-03-20 16:40:58 +0800

    tianjia

---
+++ Client/Program.cs
@@ -0,0 +1,56 @@
+﻿
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class MainClass
+    {
+        public static   void Main(string[] args){
+
+            var disco =   DiscoveryClient.GetAsync("http://localhost:5000").Result;
+             
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            //为API请求令牌
+            var tokenResponse =   tokenClient.RequestClientCredentialsAsync("api1").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response =   client.GetAsync("http://localhost:5001/identity").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content =   response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+       
+    }
+}
---
+++ IdentityServerProject/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServerProject
+{
+    public class Config
+    {
+        public Config()
+        {
+            
+        }
+
+        public static IEnumerable<ApiResource> GetApiResource(){
+            return new List<ApiResource>(){
+                 new ApiResource("api1","MyApi")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                 new Client
+                 {
+                     ClientId = "client",
+                
+                     // no interactive user, use the clientid/secret for authentication
+                     AllowedGrantTypes = GrantTypes.ClientCredentials,
+                
+                     // secret for authentication
+                     ClientSecrets =
+                     {
+                         new Secret("secret".Sha256())
+                     },
+                
+                     // scopes that client has access to
+                     AllowedScopes = { "api1" }
+                 }
+             };
+        }
+
+
+    }
+}
